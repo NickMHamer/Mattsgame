a25f765afaee3464ec3232b810671d5258bc364f
Nick <nhamer233@gmail.com>
2021-11-21 18:16:13 -0500

    first commit

---
+++ Kidsgame.js
@@ -0,0 +1,5 @@
+(function () {
+  const gameType = document.querySelector("select");
+  const startButton = document.querySelector("#startButton");
+  const stopButton = document.querySelector("#stopButton");
+})();
